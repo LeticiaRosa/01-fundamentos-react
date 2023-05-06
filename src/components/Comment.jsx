19284a4936d1e7fbbf25a27f1876fdbf674c6f8f
Leticia Rosae <leticiarosadeoliveira@yahoo.com.br>
2023-05-05 23:08:43 -0300

    Finalized the list of comments

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="https://github.com/LeticiaRosa.png"
+        alt=""
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time
+                title=" 04 de Maio de 2023 às 22:04:15"
+                dateTime="2023-05-04 22:04:15"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deleta Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import styles from "./Post.module.css";
+import { Comment } from "./Comment";
 
 export function Post({ author, content }) {
   return (
@@ -49,6 +50,11 @@ export function Post({ author, content }) {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
